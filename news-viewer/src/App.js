44b920a50c3a01c79a583ada8f69158fee01cc35
박상민 <iamsangminpark@gmail.com>
2021-04-11 13:21:53 +0900

    category styling

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState } from 'react';
+// import axios from 'axios';
 
-function App() {
+// const App = () => {
+//   const [data, setData] = useState(null);
+//   const onClick = async () => {
+//     // axios
+//     //   .get('https://jsonplaceholder.typicode.com/todos/1')
+//     //   .then((response) => {
+//     //     setData(response.data);
+//     //   });
+//     try {
+//       const response = await axios.get(
+//         'https://newsapi.org/v2/top-headlines?country=kr&category=business&apiKey=9c600cb904a846988ebfd83a561b5c6d',
+//       );
+//       setData(response.data);
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   };
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>불러오기</button>
+//       </div>
+//       {data && (
+//         <textarea
+//           rows={7}
+//           value={JSON.stringify(data, null, 2)}
+//           readOnly={true}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React, { useState, useCallback } from 'react';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
+
+const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />;
+    </>
   );
-}
+};
 
 export default App;
