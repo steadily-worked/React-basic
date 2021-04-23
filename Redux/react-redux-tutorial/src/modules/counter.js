464a1e0f4217c3f2a383ce5d1c00a7270fe7e11a
박상민 <iamsangminpark@gmail.com>
2021-04-23 17:28:28 +0900

    액션 생성함수

---
+++ Redux/react-redux-tutorial/src/modules/counter.js
@@ -0,0 +1,5 @@
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+export const increase = () => ({ type: 'INCREASE' });
+export const decrease = () => ({ type: 'DECREASE' });
