765034f500e753729689b7e093b7656d28b44abf
박상민 <iamsangminpark@gmail.com>
2021-05-05 19:00:31 +0900

    add Switch

--- spa-using-react-router/src/App.js
+++ spa-using-react-router/src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import About from "./components/About";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
-import { Route, Link } from "react-router-dom";
+import Profiles from "./components/Profiles";
+import HistorySample from "./components/HistorySample";
+import { Route, Link, Switch } from "react-router-dom";
 
 const App = () => {
   return (
@@ -20,11 +22,29 @@ const App = () => {
         <li>
           <Link to="/profile/honggildong">홍길동 프로필</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">히스토리</Link>
+        </li>
       </ul>
       <hr />
-      <Route path="/" component={Home} exact={true} />
-      <Route path={["/info", "/about"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path={["/info", "/about"]} component={About} />
+        <Route path="/profile/:username" component={Profile} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다.</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 };
