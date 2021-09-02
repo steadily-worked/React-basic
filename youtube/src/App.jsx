b3040a83e943c9b1742d198bb22809978faafeca
박상민 <iamsangminpark@gmail.com>
2021-09-02 20:55:20 +0900

    init project using YouTube APIs

---
+++ youtube/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import VideoList from "./components/video_list/video_list";
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAtrSZkopq--QXlpEYQ5SrM9Kg5TZlZMl0",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
+}
+
+export default App;
---
+++ youtube/src/components/video_item/video_item.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+
+export default VideoItem;
