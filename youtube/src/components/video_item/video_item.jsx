474deb3a6bcc95393b819e9a9cfc0ec1f9544f18
박상민 <iamsangminpark@gmail.com>
2021-09-02 21:55:36 +0900

    apply PostCSS

--- youtube/src/components/video_item/video_item.jsx
+++ youtube/src/components/video_item/video_item.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import styles from "./video_item.module.css";
 
-const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+const VideoItem = ({ video: { snippet } }) => (
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img
+        className={styles.thumbnail}
+        src={snippet.thumbnails.medium.url}
+        alt="video thumbnail"
+      />
+      <div className={styles.metadata}>
+        <p className={styles.title}>{snippet.title}</p>
+        <p className={styles.channel}>{snippet.channelTitle}</p>
+      </div>
+    </div>
+  </li>
+);
 
 export default VideoItem;
--- youtube/src/components/video_list/video_list.jsx
+++ youtube/src/components/video_list/video_list.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import VideoItem from "../video_item/video_item";
 
-const VideoList = (props) => {
+const VideoList = (props) => (
   <ul>
     {props.videos.map((video) => (
       <VideoItem key={video.id} video={video} />
     ))}
-  </ul>;
-};
+  </ul>
+);
 
 export default VideoList;
