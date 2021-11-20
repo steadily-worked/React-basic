0746f4a493d968f36e7162c9e77c95d469001109
박상민 <iamsangminpark@gmail.com>
2021-11-20 16:30:18 +0900

    add

--- 62_fe_d80d_bded_42a0_a42b_5b546a4fac4b_ide_1637382012/src/App.js
+++ 62_fe_d80d_bded_42a0_a42b_5b546a4fac4b_ide_1637382012/src/App.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
-import logo from './logo.svg';
-import Timer from './components/timer/index.js';
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+import Timer from "./components/timer/index.js";
 
 const title = "Timer";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="app-header layout-row align-items-center justify-content-center">
-          <div className="layout-row align-items-center">
-            <img alt="" src={logo} className="logo"/>
-            <h4 id="app-title" data-testid="app-title" className="app-title">{title}</h4>
-          </div>
-        </nav>
-        <Timer initial={this.props.initial}/>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <nav className="app-header layout-row align-items-center justify-content-center">
+        <div className="layout-row align-items-center">
+          <img alt="" src={logo} className="logo" />
+          <h4 id="app-title" data-testid="app-title" className="app-title">
+            {title}
+          </h4>
+        </div>
+      </nav>
+      <Timer />
+    </div>
+  );
+};
 
 export default App;
