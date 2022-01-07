615c6b82c3c75c38a3343b81a25727e627f855a2
steadily-worked <iamsangminpark@gmail.com>
2022-01-07 14:46:52 +0900

    add: bump effect

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,37 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useState, useEffect } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "components/Cart/CartIcon";
 import CartContext from "store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
   const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => {
     return currentNum + item.amount;
   }, 0);
 
+  const { items } = cartCtx;
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
     <Fragment>
-      <button className={classes.button} onClick={props.onClick}>
+      <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
           <CartIcon />
         </span>
