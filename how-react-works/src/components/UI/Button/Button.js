0d4e37972448f03eafe776d2b9b0c4c6972123c9
steadily-worked <iamsangminpark@gmail.com>
2022-01-09 14:51:26 +0900

    add: useCallback & React.memo

--- how-react-works/src/components/UI/Button/Button.js
+++ how-react-works/src/components/UI/Button/Button.js
@@ -1,11 +1,11 @@
-import React from 'react';
-
-import classes from './Button.module.css';
+import React from "react";
+import classes from "./Button.module.css";
 
 const Button = (props) => {
+  console.log("BUTTON RUNNING");
   return (
     <button
-      type={props.type || 'button'}
+      type={props.type || "button"}
       className={`${classes.button} ${props.className}`}
       onClick={props.onClick}
       disabled={props.disabled}
@@ -15,4 +15,4 @@ const Button = (props) => {
   );
 };
 
-export default Button;
+export default React.memo(Button);
