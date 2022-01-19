72ea74d7cdb29b1b15f4b02ce66bcd0e32be7867
steadily-worked <iamsangminpark@gmail.com>
2022-01-19 21:11:04 +0900

    ☔test: detecting text on click event

--- react-testing/src/components/Greeting.test.js
+++ react-testing/src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 // 1 suite with 1 test.
@@ -11,10 +12,33 @@ describe("Greeting Component", () => {
     // ... nothing
 
     // 3. Assert
-    // screen.find(): 에러 발생 시 promise를 반환 -> 화면상에 엘리먼트가 나타나게 됨
     const helloWorldElement = screen.getByText("Hello World", {
       exact: false /* 완벽하게 똑같은지 여부 검사, 디폴트 값이 true임 */,
     });
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("버튼을 클릭하지 않았을 때 '만나서 반가워요!'가 렌더링 되는지 검사해볼게요", () => {
+    render(<Greeting />);
+
+    const paragraphElement = screen.getByText("만나서 반가워요", {
+      exact: false,
+    });
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  test("버튼을 클릭했을 때 '이렇게 바뀌었어요!'가 렌더링 되는지 검사해볼게요", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const changedTextElement = screen.getByText("이렇게 바뀌었어요", {
+      exact: false,
+    });
+    expect(changedTextElement).toBeInTheDocument();
   });
 });
